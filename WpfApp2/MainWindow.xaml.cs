b4eba2992ffb08431f35637aec35492eae868210
Hegedus-Gergo <71663811+Hegedus-Gergo@users.noreply.github.com>
2023-04-27 13:31:27 +0200

    Add files via upload

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using WpfAppSQLTermekek;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string kapcsolatLeiro = "datasource=127.0.0.1;port=3306;username=root;password=;database=hardver;charset=utf8;";
+        List<Termek> termekek = new List<Termek>();
+        MySqlConnection SQLkapcsolat;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AdatbazisMegnyitas();
+            KategoriaBetoltese();
+            GyartokBetoltese();
+
+            TermekekBetolteseListaba();
+
+            AdatbazisLezarasa();
+        }
+
+        private void btnSzukit_Click(object sender, RoutedEventArgs e)
+        {
+            termekek.Clear();
+            string SQLSzukitettLista= SzukitoLekerdezesEloallitasa();
+            MySqlCommand SQLparancs = new MySqlCommand(SQLSzukitettLista, SQLkapcsolat);
+            MySqlDataReader eredmenyolvaso = SQLparancs.ExecuteReader();
+            while (eredmenyolvaso.Read())
+            {
+                Termek uj = new Termek(eredmenyolvaso.GetString("Kategória"), eredmenyolvaso.GetString("Gyártó"),eredmenyolvaso.GetString("Név"), eredmenyolvaso.GetInt32("Ár"), eredmenyolvaso.GetInt32("Garidő"));
+                termekek.Add(uj);
+            }
+            eredmenyolvaso.Close();
+            dgTermekek.Items.Refresh();
+              
+        }
+
+        private void btnMentes_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("blabla.csv");
+            foreach (var item in termekek);
+            {
+                sw.WriteLine(ItemCollection.toCSVString());
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ WpfApp2/Termek.cs
@@ -0,0 +1,64 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppSQLTermekek
+{
+    public class Termek
+    {
+        string kategoria;
+        string gyarto;
+        string nev;
+        int ar;
+        int garido;
+
+        public Termek(string kategoria, string gyarto, string nev, int ar, int garido)
+        {
+            this.kategoria = kategoria;
+            this.gyarto = gyarto;
+            this.nev = nev;
+            this.ar = ar;
+            this.garido = garido;
+        }
+
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+        public string Gyarto { get => gyarto; set => gyarto = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int Ar { get => ar; set => ar = value; }
+        public int Garido { get => garido; set => garido = value; }
+
+    }
+    public void TermekekBetolteseListaba() { 
+    string SQLOsszesTermek = "SELECT * FROM termékek;";
+        MySqlCommand SQLparancs = new MySqlCommand(SQLOsszesTermek, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+
+        while (eredmenyOlvaso.Read())
+        {
+            Termek uj = new Termek(eredmenyOlvaso.GetString("Kategória"), eredmenyOlvaso.GetString("Gyártó"), eredmenyOlvaso.GetString("Név"), eredmenyOlvaso.GetInt32("Ár"), eredmenyOlvaso.GetInt32("Garidő"));
+            termekek.Add(uj);
+        }
+        eredmenyOlvaso.Close();
+        dgTermekek.ItemSource = termekek;
+    }
+    private void KategoriaBetoltese() {
+        string SQLKategoriakRendezve = "SELECT DISTINCT kategória FROM termékek ORDER BY kategória;";
+        MySqlCommand SQLparancs = new MySqlCommand(SQLKategoriakRendezve, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+        cbKategoria.Items.Add("- Nincs megadva -");
+        while (eredmenyOlvaso.Read()) {
+            cbKategoria.Items.Add(eredmenyOlvaso.GetString("kategória"));
+        }
+        eredmenyOlvaso.Close();
+        cbKategoria.SelectedIndex = 0;
+
+    }
+    private void GyartokBetoltese() {
+        string SQLGyartokRendezve = "SELECT DISTINCT gyártó FROM termékek ORDER BY gyártó;";
+        MySqlCommand SQLparancs = new MySqlCommand(SQLKategoriakRendezve, SQLkapcsolat);
+        MySqlDataReader eredmenyOlvaso = SQLparancs.ExecuteReader();
+    }
+}
